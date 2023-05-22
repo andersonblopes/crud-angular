63f148f8b706bf35228ecf31a2db92ad6a9bb864
Anderson Lopes <>
2023-05-22 22:55:31 +0200

    Improves user module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,19 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users/users.component';
 
-
 @NgModule({
-  declarations: [
-    UsersComponent
-  ],
+  declarations: [UsersComponent],
   imports: [
     CommonModule,
     UsersRoutingModule,
-    MatTableModule
-  ]
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
 })
-export class UsersModule { }
+export class UsersModule {}
