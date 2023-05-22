b85099db80b181b08414afdc2579212271b01767
Anderson Lopes <>
2023-05-22 22:38:54 +0200

    Includes list of users

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] = [
+    {
+      id: '1',
+      name: 'Anderson',
+      email: 'anderson@email.com',
+      createdAt: new Date(),
+    },
+    {
+      id: '2',
+      name: 'AHelena',
+      email: 'helena@email.com',
+      createdAt: new Date(),
+    },
+    {
+      id: '3',
+      name: 'Elaine',
+      email: 'elaine@email.com',
+      createdAt: new Date(),
+    },
+  ];
+  displayedColumns = ['name', 'email', 'createdAt'];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
