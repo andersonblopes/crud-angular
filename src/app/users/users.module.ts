32693d7b64c80d0b092a0c90ea7614484da90461
Anderson Lopes <>
2023-05-22 23:02:23 +0200

    Improves modules organization

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,20 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [UsersComponent],
-  imports: [
-    CommonModule,
-    UsersRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-  ],
+  imports: [CommonModule, UsersRoutingModule, AppMaterialModule],
 })
 export class UsersModule {}
