2e31db1632db846d7538aee37f04140d7f78463f
Anderson Lopes <>
2023-05-22 23:03:56 +0200

    Improves packages

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from '../model/user';
+import { User } from '../../shared/model/user';
 
 @Component({
   selector: 'app-users',
